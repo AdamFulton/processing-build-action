a1a00f010f231bc2c8f9abe5bb04427209db6957
Adam Fulton <adam.fulton1@students.mq.edu.au>
2024-05-07 14:36:18 +1000

    fixed up formatting and added function comment

--- src/processingHelper.js
+++ src/processingHelper.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-const path = require('path');
-const { exec } = require('child_process');
-const util = require('util');
+const fs = require("fs");
+const path = require("path");
+const { exec } = require("child_process");
+const util = require("util");
 const execPromise = util.promisify(exec);
 /**
  * Finds all Processing sketches in a directory
@@ -9,47 +9,43 @@ const execPromise = util.promisify(exec);
  * @param {Array} sketchPaths - The array to store the paths of the Processing sketches
  * @returns {Array} - An array of paths to Processing sketches
  */
-function findProcessingSketches(dir,sketchPaths = []) {
+function findProcessingSketches(dir, sketchPaths = []) {
+  const files = fs.readdirSync(dir);
 
-    const files = fs.readdirSync(dir);
+  for (const file of files) {
+    const filePath = path.join(dir, file);
 
-    for (const file of files) {
+    const stats = fs.statSync(filePath);
 
-        const filePath = path.join(dir, file);
-
-        const stats = fs.statSync(filePath);
-
-        if (stats.isDirectory()) {
-
-            findProcessingSketches(filePath,sketchPaths);
-
-        } else if (file.endsWith('.pde')) {
-
-            sketchPaths.push(dir);
-        }
+    if (stats.isDirectory()) {
+      findProcessingSketches(filePath, sketchPaths);
+    } else if (file.endsWith(".pde")) {
+      sketchPaths.push(dir);
     }
-    return sketchPaths;
+  }
+  return sketchPaths;
 }
 
+/**
+ * Builds Processing sketches using the processing-java command
+ * @param {*} sketches 
+ * @returns {Array} - An array of errors that occurred during the build process
+ */
 async function buildProcessing(sketches) {
-    
-        let errors = []
+  let errors = [];
 
-        for (const sketch of sketches) {
-            const command = `processing-java --sketch=${sketch} --build`;
+  for (const sketch of sketches) {
+    const command = `processing-java --sketch=${sketch} --build`;
 
-            try {
-               
-                await  execPromise(command);
-              
-            } catch (error) {
+    try {
+      await execPromise(command);
+    } catch (error) {
+      errors.push({
+        message: error.stderr,
+        cmd: error.cmd,
+      });
+    }
+  }
 
-                errors.push({
-                    message: error.stderr,
-                    cmd: error.cmd, 
-                })
-            }
-        }
-
-    return errors;
+  return errors;
 }
