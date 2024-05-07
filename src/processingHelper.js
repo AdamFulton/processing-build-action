849f69ceea7a0a092a1a6c3e1331979e89f1b696
Adam Fulton <adam.fulton1@students.mq.edu.au>
2024-05-07 14:44:03 +1000

    added functions to get error message,line number, and sketch paths

--- src/processingHelper.js
+++ src/processingHelper.js
@@ -28,7 +28,7 @@ function findProcessingSketches(dir, sketchPaths = []) {
 
 /**
  * Builds Processing sketches using the processing-java command
- * @param {*} sketches 
+ * @param {*} sketches
  * @returns {Array} - An array of errors that occurred during the build process
  */
 async function buildProcessing(sketches) {
@@ -48,4 +48,48 @@ async function buildProcessing(sketches) {
   }
 
   return errors;
+}
+/**
+ * Parses the error message from the processing-java command to extract the line number
+ * @param {string} error - The error message from the processing-java command
+ * @returns {number} - The line number where the error occurred
+ */
+function getLineNumber(error) {
+  retval = error.split(":");
+
+  return retval[1];
+}
+
+/**
+ * Parses the error message from the processing-java command to extract the sketch path
+ * @param {string} cmd - The command that was run to build the Processing sketch that containts the sketch path
+ * @returns {string} - The path to the Processing sketch
+ */
+function getSketchPath(cmd) {
+  retval = cmd.split("--sketch=")[1].split(" ");
+  return retval[0];
+}
+
+/**
+ * Parses the error message from the processing-java command to extract the file name
+ * @param {string} error - The error message from the processing-java command
+ * @returns {string} - The file name where the error occurred
+ */
+function getFileName(error) {
+  retval = error.split(":");
+
+  return retval[0];
+}
+
+/**
+ * Parses the error message from the processing-java command to extract the error message
+ * @param {string} error - The error message from the processing-java command
+ * @returns {string} - The error message that occurred
+ */
+function getMessage(error) {
+  retval = error.split(":");
+  if (retval.length > 6) {
+    return retval[5] + retval[6];
+  }
+  return retval[5];
 }
